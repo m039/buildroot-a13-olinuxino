583f61bd5095916434f016345117b8f1e4301e81
Dmitry Mozgin <m0391n@gmail.com>
2013-11-08 22:39:02 +0400

    Added code to enable wifi at start up

--- board/a13_olinuxino/post-build.sh
+++ board/a13_olinuxino/post-build.sh
@@ -9,18 +9,42 @@ cp -v $BOARD_DIR/script.bin $BOOT
 if [ -e $BINARIES_DIR/u-boot.bin ];
 then
 
-    cp $BINARIES_DIR/u-boot.bin $BOOT
+	cp $BINARIES_DIR/u-boot.bin $BOOT
 
 fi
 
 if [ -e $BINARIES_DIR/sunxi-spl.bin ];
 then
 
-    cp $BINARIES_DIR/sunxi-spl.bin $BOOT
+	cp $BINARIES_DIR/sunxi-spl.bin $BOOT
 
 fi
 
 if [ -e $BOARD_DIR/wpa_supplicant.conf ];
 then
-    cp $BOARD_DIR/wpa_supplicant.conf $TARGET_DIR/etc/wpa_supplicant.conf
+
+	cp $BOARD_DIR/wpa_supplicant.conf $TARGET_DIR/etc/wpa_supplicant.conf
+
+fi
+
+
+if [ -e $BOARD_DIR/modprobe.conf ];
+then
+
+	cp $BOARD_DIR/modprobe.conf $TARGET_DIR/etc/modprobe.conf
+
+fi
+
+if [ -e $BOARD_DIR/inerfaces ];
+then
+
+	cp $BOARD_DIR/interfaces $TARGET_DIR/etc/network/interfaces
+
+fi
+
+if [ -e $BOARD_DIR/wpa_supplicant.conf ];
+then
+
+	cp $BOARD_DIR/wpa_supplicant.conf $TARGET_DIR/etc/wpa_supplicant.conf
+
 fi
